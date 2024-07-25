9d00c3d81676eb23aed6e48cdd2e05c3ec6c302d
ishaan2809 <ishaan2809@gmail.com>
2024-07-25 22:15:31 +0930

    assignment-1

--- main.cpp
+++ main.cpp
@@ -6,17 +6,16 @@
 
 int main(){
     Human player1;
-    Human player2("Wei");
     Computer computer;
     Referee referee;
-    Player * winner = referee.refGame(&player2, &computer);
-    if(winner == nullptr){
-        std::cout<<"It's a Tie"<< std::endl;
-    }
-        else{
-            std::cout<< winner->getName() << "You win"<< std::endl;
+    Player * winner = referee.refGame(&player1, &computer);
+    if (winner == nullptr) {
+            std::cout << "It's a Tie" << std::endl;
+        } else if (winner == &player1) {
+            std::cout << " You win" << std::endl;
+        } else {
+            std::cout << "You lose" << std::endl;
         }
-        return 0;
 
 
 
