5220b220a67ed76a84b45fa01d7b8990d5cb8c2a
ishaan2809 <ishaan2809@gmail.com>
2024-08-03 13:13:51 +0930

    adds-2

--- Referee.cpp
+++ Referee.cpp
@@ -10,17 +10,14 @@ Player *Referee::refGame(Player *player1, Player *player2)
     Move* move2 = player2->makeMove();
     
     if (move1->getName() == move2->getName()) {
-        delete move1;
-        delete move2;
+     
         return nullptr;
-        
+
     } else if (move1->beats(move2)) {
-        delete move1;
-        delete move2;
+       
         return player1;
     } else {
-        delete move1;
-        delete move2;
+      
         return player2;
     }
 }
