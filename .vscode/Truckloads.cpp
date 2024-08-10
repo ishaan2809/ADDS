17fc2662b7d591d7e50728b75011faa928760cc0
ishaan2809 <ishaan2809@gmail.com>
2024-08-10 14:31:10 +0930

    adds-3

--- .vscode/Truckloads.cpp
+++ .vscode/Truckloads.cpp
@@ -1,4 +1,4 @@
-#include <iostream>git branch
+#include <iostream>
 
 
 class Truckloads{
--- Reverser.cpp
+++ Reverser.cpp
@@ -28,16 +28,5 @@ public:
     }
 };
 
-int main() {
-    Reverser r;
-    // Test reverseDigit
-    int reversedNumber = r.reverseDigit(12345);
-    std::cout << "Reversed Number: " << reversedNumber << std::endl;  // Should print "54321"
 
-    // Test reverseString
-    std::string reversedString = r.reverseString("hello");
-    std::cout << "Reversed String: " << reversedString << std::endl;  // Should print "olleh"
-
-    return 0;
-}
 
